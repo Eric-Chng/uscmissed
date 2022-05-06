f1fa7d83e29d463becafb0cedff9dfe82f19034d
Raymond Kuan <rkuan@Raymonds-MacBook-Pro.local>
2022-05-05 23:04:27 -0700

    Added approve/reject servlet

---
+++ Missed Connections/src/main/java/servlets/RejectServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import data.Database;
+
+/**
+ * Servlet implementation class RejectServlet
+ */
+@WebServlet("/RejectServlet")
+public class RejectServlet extends HttpServlet {
+	private static final long serialVersionUID = 7507777331673090255L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RejectServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int post_id = XXX; //get the post_id from the jsp page
+		Database db = new Database();
+		db.reject_post(post_id);
+		
+		request.getRequestDispatcher("/admin.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
